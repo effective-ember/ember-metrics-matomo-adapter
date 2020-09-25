f067c20febef412ab9a214921dd95d27d7c20479
Michael Klein <michael@firstiwaslike.com>
2020-09-25 15:03:48 +0200

    chore: ts:precompile for ts consumers

---
+++ metrics-adapters/matomo.d.ts
@@ -0,0 +1,32 @@
+import Base from 'ember-metrics/metrics-adapters/base';
+declare type MatomoTrackPageOptions = {
+  page: string;
+  title: string;
+};
+declare type MatomoTrackEventOptions = {
+  category: string;
+  action: string;
+  name?: string;
+  value?: number;
+};
+declare type MatomoConfig = {
+  scriptUrl: string;
+  trackerUrl: string;
+  siteId: string;
+  disableCookies: boolean;
+};
+interface MatomoClient {
+  push: (params: any[]) => void;
+}
+declare global {
+  interface Window {
+    _paq?: MatomoClient;
+  }
+}
+export default class MatomoAdapter extends Base<MatomoConfig> {
+  init(): void;
+  trackPage({ page, title }: MatomoTrackPageOptions): void;
+  trackEvent({ category, action, name, value }: MatomoTrackEventOptions): void;
+  willDestroy(): void;
+}
+export {};
